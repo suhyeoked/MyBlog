7f62bd824dbfe7a208596837ccd6f42188818ac4
choisuhyeok <suhyeok00@naver.com>
2025-04-06 15:01:10 +0900

    vvv

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,59 @@
-import { useState } from 'react'
-import './App.css'
-import NabiBa from './component/NabiBa'
-import {Link } from "react-router-dom";
+import React, { useState } from 'react';
+import './App.css';
+import NabiBa from './component/NabiBa';
+import { Link } from "react-router-dom";
+import BlogWriting from './component/BlogWriting';
 
 function App() {
-  let [good , setGood] = useState<number>(0)
-
+  let [title, setTitle] = useState<string[]>(["예시 1"]);
+  let [blogBody , setBlogBody] = useState<string[]>([])
+  let [good, setGood] = useState<number[]>(new Array(title.length).fill(0));
+  let [isWriting, setIsWriting] = useState<boolean>(false);
+  
   return (
     <>
-     <NabiBa/>
-     <section className="Blog">
-      <div className="BlogTitle">
-      <h1>
-        title
-      </h1>
-      <span className="GoodBtn">
-        <button onClick={()=>{
-          setGood(good + 1)
-        }}>
-          공감
-        </button>
-      </span>
-      <span>
-        {good}
-      </span>
-      </div>
-      <p>
-        title-Body
-      </p>
-      <p>
-        발행 날짜
-      </p>
-     </section>
-     <section className="BlogFooter">
-      <div>
-        <Link to="/BlogWriting">
-        글쓰기
-        </Link>
-        </div>
-     </section>
+      <NabiBa />
+      {
+        title.map((title, i) => {
+          return (
+            <section className="Blog">
+              <div className="BlogTitle">
+                <Link to="/" target="_blank">
+                <h1>{title}</h1>
+                </Link>
+                <span className="GoodBtn">
+                  <button onClick={() =>{
+                    let copy = [...good]
+                    copy[i] += 1
+                    setGood(copy)
+                  } 
+                  }>
+                    공감
+                  </button>
+                </span>
+                <span>
+                  {good[i]}
+                </span>
+              </div>
+            </section>
+          );
+        })
+      }
+      <section className="BlogFooter">
+        <Link to = "./BlogWriting" target="_blank">
+          글쓰기
+          </Link>
+      {isWriting && (
+        <BlogWriting 
+          title={title} 
+          setTitle={setTitle} 
+          blogBody={blogBody} 
+          setBlogBody={setBlogBody}
+        />
+      )}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/component/BlogWriting.tsx
+++ src/component/BlogWriting.tsx
@@ -1,22 +1,47 @@
-import React from 'react'
+import React, { createContext, useContext, useState } from 'react';
 import NabiBa from './NabiBa'
 import './BlogWriting.css'
 
-function BlogWriting() {
+interface BlogWritingInterface {
+  title : string[]
+  setTitle : React.Dispatch<React.SetStateAction<string[]>>
+  blogBody : string[]
+  setBlogBody : React.Dispatch<React.SetStateAction<string[]>> 
+  textBlog : string
+  setTextBlog : React.Dispatch<React.SetStateAction<string>> 
+}
+
+function BlogWriting(props : BlogWritingInterface) {
+
   return (
     <>
       <NabiBa />
       <div className="BlogWriting">
         <div className="BlogWritingTitle">
-          <input type="text" placeholder='제목을 입력하시오' maxlength={10}/>
-          <textarea className="BlogWritingBody" placeholder='내용을 입력하세요' rows={10} cols={50}/>
-        </div>
-      </div>
-      <div className="BlogWritingSuccess">
-          <button>
+          <input type="text" placeholder='제목을 입력하시오' maxLength={10}
+          onChange={(e)=>{
+            props.setTextBlog(e.target.value)
+            console.log(e.target.value)
+          }}
+          />
+          <textarea className="BlogWritingBody" placeholder='내용을 입력하세요' rows={10} cols={50} 
+          onChange={(e)=>{
+            props.setTextBlog(e.target.value)
+            console.log(e.target.value)
+          }}/>
+          <div className="BlogWritingSuccess">
+          <button onClick={()=>{
+            let copy = [...props.title]
+            copy.unshift(props.textBlog)
+            props.setTextBlog(copy)
+          }}>
             작성 완료
           </button>
       </div>
+        </div>
+        
+      </div>
+      
     </>
   )
 }
